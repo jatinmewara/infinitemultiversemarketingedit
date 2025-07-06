2513b7f51a01a075440e9c262e6a88147006fecd
Jatin Mewara <jatinmewara25@gmail.com>
2025-07-06 20:45:13 +0530

    newone

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -75,7 +75,7 @@
                 <li class="nav-item {{ Request::is('users') ? 'active' : '' }}">
                     <a href="users" class="nav-link">
                         <i class="fas fa-user"></i>
-                        <span class="nav-text">Userss</span>
+                        <span class="nav-text">Userssciqwubdquwi</span>
                     </a>
                 </li>
                 <!-- <li class="nav-item">
